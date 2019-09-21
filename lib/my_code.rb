4fc40e7880f1c8f30032654433bf9040fe61f2f2
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-21 22:45:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,34 @@ def map (source_array)
 end
 
 
+def reduce (source_array, starting_point = 0)
+  total = starting_point
+  i = 0
+  while i < source_array.length  
+    total+= yield(source_array[i])
+    i+=1 
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+i = 0
+  while i < source_array.length do
+    if source_array[i] == false
+      return false
+    end
+    i+=1
+  end 
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == true
+      return true
+    end
+    i+=1
+  end 
+  return false
+end
